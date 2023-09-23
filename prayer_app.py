cebdbf2657bea9a9ef18e0016dda4e12cf05acb4
Moe-Dahan <83793097+Moe-Dahan@users.noreply.github.com>
2023-09-23 13:14:39 +1000

    Update prayer_app.py

--- prayer_app.py
+++ prayer_app.py
@@ -5,74 +5,13 @@ import pygame
 from plyer import notification
 import time
 import os
-import shutil
-import sys
 import multiprocessing
 import requests
 import json
-
+from PIL import Image, ImageDraw
+import pystray
 
 sg.theme("SystemDefault")
-
-''' adding the program to startup '''
-def add_to_startup_windows():
-    script_path = os.path.abspath(sys.argv[0])
-    startup_folder = os.path.join(os.environ['APPDATA'], 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
-    target_path = os.path.join(startup_folder, os.path.basename(script_path))
-    if not os.path.exists(target_path):
-        shutil.copy2(script_path, target_path)
-
-def add_to_startup_macos():
-    script_path = os.path.abspath(sys.argv[0])
-    startup_folder = os.path.join(os.path.expanduser("~"), 'Library', 'LaunchAgents')
-    plist_filename = os.path.basename(script_path).replace('.py', '.plist')
-    plist_path = os.path.join(startup_folder, plist_filename)
-    plist_content = f"""<?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-        <key>Label</key>
-        <string>{os.path.basename(plist_filename)}</string>
-        <key>ProgramArguments</key>
-        <array>
-            <string>{sys.executable}</string>
-            <string>{script_path}</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-    </dict>
-    </plist>
-    """
-    with open(plist_path, 'w') as plist_file:
-        plist_file.write(plist_content)
-
-''' linux startup config '''
-def add_to_startup_linux():
-    script_path = os.path.abspath(sys.argv[0])
-    desktop_path = os.path.join(os.path.expanduser("~"), '.config', 'autostart', os.path.basename(script_path) + '.desktop')
-    desktop_content = f"""[Desktop Entry]
-    Type=Application
-    Exec={sys.executable} {script_path}
-    Hidden=false
-    NoDisplay=false
-    X-GNOME-Autostart-enabled=true
-    Name[en_US]={os.path.basename(script_path)}
-    Name={os.path.basename(script_path)}
-    Comment[en_US]=Your script description
-    Comment=Your script description
-    """
-    with open(desktop_path, 'w') as desktop_file:
-        desktop_file.write(desktop_content)
-
-
-if os.name == 'posix':
-    try:
-        add_to_startup_linux()
-    except OSError:
-        add_to_startup_macos()
-elif os.name == 'nt':
-    add_to_startup_windows()
-
 
 def play_azan(name):
     pygame.mixer.init()
@@ -82,13 +21,11 @@ def play_azan(name):
     title = name
     message = f'Time to Pray {name}'
     notification.notify(title=title, message=message, timeout=10)
-    time.sleep(120) # make it longer with the pause
+    time.sleep(120) 
     pygame.mixer.music.stop()
     pygame.mixer.quit()
 
-
 def get_state_country():
-        # Create a PySimpleGUI window to get user input
     layout = [
         [sg.Text("Enter State: "), sg.InputText(key="state")],
         [sg.Text("Enter Country: "), sg.InputText(key="country")],
@@ -96,78 +33,122 @@ def get_state_country():
     ]
 
     window = sg.Window("State and Country Input", layout, icon='minaret_prayer_call_sound_islam_adzan_icon_251083.ico')
-
+    
     while True:
         event, values = window.read()
         if event == sg.WINDOW_CLOSED:
             break
         elif event == "Submit":
-            window.close()
-            state = values["state"]
-            country = values["country"]
-            locations = {"state" : state, "country" : country}
-            with open("location.json", 'w') as file:
-                json.dump(locations, file)
-            if state and country:
-                url = f"https://aladhan.com/play/{state}/{country}"        
-    return url
+            try:
+                window.close()
+                state = values["state"]
+                country = values["country"]
+                locations = {"state" : state, "country" : country}
+                with open("location.json", 'w') as file:
+                    json.dump(locations, file)
+                if state and country:
+                    url = f"https://aladhan.com/play/{state}/{country}"        
+                return url
+            except UnboundLocalError:
+                print("Error")
+                return get_state_country()
+            
+''' creating the system icon '''
+def create_image(width, height, color2):
+    # Generate an image and draw a pattern
+    image = Image.open('minaret_prayer_call_sound_islam_adzan_icon_251083.png')
+    dc = ImageDraw.Draw(image)
+    dc.rectangle((width // 2, 0, width, height // 2), fill=color2)
+    dc.rectangle((0, height // 2, width // 2, height),fill=color2)
+    return image
 
-def gui_process(url):
-    url = requests.get(url) # add your state/suburb and country
-    soup = BeautifulSoup(url.content, 'html.parser')
+def on_clicked(icon, item):
+    # Execute different actions based on the menu item text
+    if item.text == 'Edit location':
+        return get_state_country()
+    elif item.text == 'Close':
+        icon.stop()
+    elif item.text == 'times':
+        try:
+            scraper = Praying_times_scraper(url=f"https://aladhan.com/play/{file['state']}/{file['country']}")
+            scraper.small_popup_window()
+        except TypeError:
+            scraper = Praying_times_scraper(url=get_state_country())
+            scraper.small_popup_window()
+# Create the menu items
+menu_items = [
+    pystray.MenuItem("times", lambda icon, item: on_clicked(icon, item)),
+    pystray.MenuItem('Edit location', lambda icon, item: on_clicked(icon, item)),
+    pystray.MenuItem('Close', lambda icon, item: on_clicked(icon, item))
+]
 
-    prayer = {}
+icon = pystray.Icon('Prayer Times', create_image(64, 64, 'black'), 'Prayer Times', menu_items)
 
-    prayer_names = soup.find_all('dt')
-    prayer_times = soup.find_all('dd')
-
-    prayer_display = [] # to display the output praying times and names
-
-    # Loop through each pair of prayer names and times, and store them in the dictionary
-    for name, time in zip(prayer_names, prayer_times):
-        prayer[name.text] = time.text
-        prayer_display.append([sg.Text(name.text, key=name.text, font=('Helvetica', 10, 'bold')), sg.Text(prayer[name.text], key=prayer[name.text], font=('Helvetica', 10, 'bold'))])
-
-    ''' sets the main gui section here '''
-    pray_frame = [
-        [sg.Text("Current Time", font=('Helvetica', 10, 'bold')), sg.Text("", key='time_now', font=('Helvetica', 10, 'bold'))],
-        [sg.Frame("Prayer Times", layout=prayer_display)]
-    ]
-
-    window = sg.Window("", pray_frame, grab_anywhere=True, icon='minaret_prayer_call_sound_islam_adzan_icon_251083.ico', titlebar_icon='adhan-call.png', no_titlebar=True)#'muslim_carpet_prayer_mat_sajadah_icon_251091.ico')
-
-    bg_process = multiprocessing.Process(target=play_azan, args=("Prayer Name",))
-    bg_process.daemon = True
+class Praying_times_scraper:
+    def __init__(self, url):
+        self.url = url                
     
-    while True:
-        event, value = window.read(timeout=1000)
-        if event == sg.WINDOW_CLOSED:
-            break
-        time_now = datetime.datetime.now().strftime('%H:%M')
-        window['time_now'].update(time_now)
-        for name, time_text in prayer.items():
-            prayer_time = datetime.datetime.strptime(time_text, '%H:%M').time()
-            current_time = datetime.datetime.strptime(time_now, '%H:%M').time()
-            if prayer_time == current_time:
-                bg_process.start()
-                window[name].update(text_color="red")
-                window[time_text].update(text_color="red")
-                play_azan(name)
-                return True
-            elif prayer_time > current_time:
-                window[name].update(text_color="green")
-                window[time_text].update(text_color="green")
-            else:
-                window[name].update(text_color="red")
-                window[time_text].update(text_color="red")
+    def small_popup_window(self):
+        url = requests.get(self.url)
+        soup = BeautifulSoup(url.content, 'html.parser')
 
-    window.close()
+        prayer = {}
 
-if __name__ == '__main__':
+        prayer_names = soup.find_all('dt')
+        prayer_times = soup.find_all('dd')
+
+        for name, time in zip(prayer_names, prayer_times):
+            prayer[name.text] = time.text
+
+        prayer_display = []
+
+        for name, time in prayer.items():
+            prayer_display.append([sg.Text(name, key=name, font=('Helvetica', 10, 'bold')), 
+                                   sg.Text(time, key=time, 
+                                           font=('Helvetica', 10, 'bold'))])
+            
+
+        pray_frame = [
+            [sg.Text("Current Time", font=('Helvetica', 10, 'bold')), sg.Text("", key='time_now', font=('Helvetica', 10, 'bold'))],
+            [sg.Frame("Prayer Times", layout=prayer_display)],
+        ]
+
+
+        screen_width, screen_height = sg.Window.get_screen_size()
+        
+        window = sg.Window("", pray_frame, grab_anywhere=False, location=(int(screen_width-200), screen_height-320),no_titlebar=False,
+                           icon='minaret_prayer_call_sound_islam_adzan_icon_251083.ico')
+
+        bg_process = multiprocessing.Process(target=play_azan, args=("Prayer Name",))
+        bg_process.daemon = True
+        
+        while True:
+            event, value = window.read(timeout=1000)
+            if event == sg.WINDOW_CLOSED:
+                return icon
+            time_now = datetime.datetime.now().strftime('%H:%M')
+            window['time_now'].update(time_now)
+            for name, time_text in prayer.items():
+                prayer_time = datetime.datetime.strptime(time_text, '%H:%M').time()
+                current_time = datetime.datetime.strptime(time_now, '%H:%M').time()
+                if prayer_time == current_time:
+                    bg_process.start()
+                    window[name].update(text_color="red")
+                    window[time_text].update(text_color="red")
+                    play_azan(name)
+                    return True
+                elif prayer_time > current_time:
+                    window[name].update(text_color="green")
+                    window[time_text].update(text_color="green")
+                else:
+                    window[name].update(text_color="red")
+                    window[time_text].update(text_color="red")
+
+if __name__ == "__main__":
     if os.path.isfile('location.json'):
         with open('location.json', 'r') as file:
             file = json.load(file)
-        gui_process(url=f"https://aladhan.com/play/{file['state']}/{file['country']}"  )
+        scraper = Praying_times_scraper(url=f"https://aladhan.com/play/{file['state']}/{file['country']}")
     else:
-        gui_process(url=get_state_country())
-    
+        scraper = Praying_times_scraper(url=get_state_country())
+    icon.run()
